1b8f7082f06271b0fa614e1e15fd369b401bde09
TranTrung142 <trantrung140219@gmail.com>
2021-03-24 09:17:07 +0700

    add file TodaySession as a sub file of Today file

---
+++ src/components/TodaySession.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+        <div class="col-2 cicle"></div>
+        <div class="col-3">
+            <div class="img" :class="[session==='Evening'?'evening':'', session==='Night'?'night':'']"></div>
+            <div class="buoi">{{session}} </div>
+        </div>
+        <div class="col-7">
+            <div class="box">
+                <div class="temp">{{tempe}}°</div>
+                <div class="inf">Warmest day of the week</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	props:{
+		tempe: {
+            type: Number,
+            default: 0,
+        },
+        session: {
+            type: String,
+            default: "",
+        },
+	}
+};
+</script>
+
+<style scoped>
+.evening{
+	background-image: url("../assets/img/rain.png") !important;
+}
+.night{
+	background-image: url("../assets/img/night.png") !important;
+}
+</style>
