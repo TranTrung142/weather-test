b44b956ea1dd9c0c70c319bb421f009e4825d67d
TranTrung142 <trantrung140219@gmail.com>
2021-03-24 09:15:53 +0700

    add file TempeSession as a sub file of Home file

---
+++ src/components/TempeSession.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="g1">
+        <div class="img" :class="[session==='Evening'?'evening':'', session==='Night'?'night':'']"></div>
+        <div class="so">{{ tempe }}°</div>
+        <div class="text">{{ session }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tempe: {
+            type: Number,
+            default: 0,
+        },
+        session: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
+
+<style scoped>
+.evening{
+	background-image: url("../assets/img/rain.png") !important;
+}
+.night{
+	background-image: url("../assets/img/night.png") !important;
+}
+
+</style>
